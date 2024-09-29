51537611798c8ee33c2773a20e60ce3665e91738
sungyoon11 <sungyoongg@gmail.com>
2024-09-29 16:55:40 +0900

    Update Ram.cpp

--- Ram.cpp
+++ Ram.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 Ram::Ram() {
 
-	size = 100 * 1024;
+	size = 100 * 1024; // 메모리 크기 설정
 	for (int i = 0; i < size; ++i)
-		mem[i] = 0;
+		mem[i] = 0;// 메모리 초기화
 }
 Ram::~Ram() {
-	cout << "¸Þ¸ð¸® Á¦°ÅµÊ";
+	cout << "메모리 제거됨";//소멸시 메세지 출력
 }
 char Ram::read(int address) {
-	return mem[address];
+	return mem[address];//주소에 값을 반환
 }
 void Ram::write(int address, char value) {
-	mem[address] = value;
+	mem[address] = value;//주소에 값을 저장
 }
