e0c34d9a48532e58446c1cfb76ce5ee466207261
sungyoon11 <sungyoongg@gmail.com>
2024-09-29 16:54:06 +0900

    Update Ram.h

--- Ram.h
+++ Ram.h
@@ -5,10 +5,10 @@ class Ram
 	int size;
 
 public:
-	Ram();
-	~Ram();
+	Ram();//생성자
+	~Ram();//소멸자
 	char read(int address);
-	void write(int address, char value);
+	void write(int address, char value);//address에 value를 저장
 
 
 };
